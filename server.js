b5939653afbd83b6a27a2c47ee28d8214dc19fc7
shally5991 <shailja5991@gmail.com>
2020-03-08 21:19:57 +0530

    added file

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = knex({
+
+	client: 'pg',
+	connection: {
+
+		host: '127.0.0.1', //local_host
+
+		user: 'postgres',
+
+		password: 'shacha',
+
+		database: 'smartbrain'
+	}
+});
+
+
+
+app.get('/',(req,res)=>{res.json('db.users') })
+
+app.post('/signin',(req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+app.post('/register',(req,res) => {register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)})
+app.put('/images',(req,res) => {image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res) => {image.handleApiCall(req,res)})
+
+
+app.listen(3001, ()=>{
+	console.log('app is runnig on 3001');
+})
