dc46e0b1834e316737a53b72673686de0cea058c
shally5991 <shailja5991@gmail.com>
2020-05-22 21:49:35 +0530

    connect to server to database

--- server.js
+++ server.js
@@ -16,14 +16,9 @@ const db = knex({
 	client: 'pg',
 	connection: {
 
-		host: '127.0.0.1', //local_host
-
-		user: 'postgres',
-
-		password: 'shacha',
-
-		database: 'smartbrain'
-	}
+		connectionString : process.env.DATABASE_URL,
+		ssl: true,
+			}
 });
 
 
